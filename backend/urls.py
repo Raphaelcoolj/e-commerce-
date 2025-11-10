40c4e792f475f96e832e50c683d4d4b617de6735
Ekeh Oghenerurie Chukwuemeka <ekehchukwuemeka375@gmail.com>
2025-11-10 14:47:36 +0100

    Add create_admin path to URLs

--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import create_admin
 from products.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -9,5 +10,6 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/', include('products.urls'))
+    path('api/', include('products.urls')),
+    path('create-admin/', create_admin)
 ]
