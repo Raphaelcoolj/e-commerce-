c860840fe2352e0f5b73104e7b3350bb2c2abf5e
Ekeh Oghenerurie Chukwuemeka <ekehchukwuemeka375@gmail.com>
2025-11-10 15:21:15 +0100

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from products.views import CreateUserView
+from products.views import CreateUserView, create_admin_user
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -9,5 +9,6 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path('api-auth/', include('rest_framework.urls')),
+    path("create-admin/", create_admin_user),
     path('api/', include('products.urls'))
 ]
