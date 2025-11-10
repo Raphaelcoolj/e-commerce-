ba2475ddecc377c59f874f536d5ca78fe0ab77be
Ekeh Oghenerurie Chukwuemeka <ekehchukwuemeka375@gmail.com>
2025-11-10 14:55:19 +0100

    Remove unused create_admin import from urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from products.views import CreateUserView, create_admin
+from products.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -9,6 +9,5 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/', include('products.urls')),
-    path('create-admin/', create_admin)
+    path('api/', include('products.urls'))
 ]
