cd071a91aaacb8563b6fd0d5b5918ea7daf0e3cd
Ekeh Oghenerurie Chukwuemeka <ekehchukwuemeka375@gmail.com>
2025-11-19 09:38:10 +0100

    Create filters.py

---
+++ products/filters.py
@@ -0,0 +1,11 @@
+import django_filters
+from rest_framework import filters
+from api.models import Product 
+
+class Product filter(django_filters.FilterSet):
+    class Meta:
+       model = Product
+       fields= {
+           'name':['iexact', 'icontain'],
+           'price':['exact', 'lt', 'gt', 'range']
+           }
