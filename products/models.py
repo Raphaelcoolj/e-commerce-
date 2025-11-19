697eee7f6c1d4d3a69b0263c5a571d84410852b3
Ekeh Oghenerurie Chukwuemeka <ekehchukwuemeka375@gmail.com>
2025-11-19 11:18:51 +0100

    Update models.py

--- products/models.py
+++ products/models.py
@@ -10,7 +10,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveIntegerField()
+    stock = models.PositiveIntegerField(default=0)
     #image = models.ImageField(upload_to='products/')
     created_at = models.DateTimeField(auto_now_add=True)
     # The 'fancy' discounted price
