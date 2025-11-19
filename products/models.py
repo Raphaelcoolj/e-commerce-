27967bcce74c7361b7a7d41bc3f5b06602be1b1d
Ekeh Oghenerurie Chukwuemeka <ekehchukwuemeka375@gmail.com>
2025-11-19 09:19:09 +0100

    Update models.py

--- products/models.py
+++ products/models.py
@@ -11,6 +11,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='products/')
     created_at = models.DateTimeField(auto_now_add=True)
+    side_price = models.DecimalField(max_digits=10, decimal_places=2,null=True, blank=True) 
 
     def __str__(self):
         return self.name
