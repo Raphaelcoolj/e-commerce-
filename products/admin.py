6b4b9864eccc8ab41f26787f8eb1bdd1c997482d
Ekeh Oghenerurie Chukwuemeka <ekehchukwuemeka375@gmail.com>
2025-11-19 10:51:52 +0100

    Update admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 from .models import Product, CartItem, Order, OrderItem
 
+
 # -------------------------------
 # Product
 # -------------------------------
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'created_at')
+    # ENHANCEMENT: Added 'stock' and 'side_price' (your discount field)
+    list_display = ('id', 'name', 'price', 'side_price', 'stock', 'created_at')
     search_fields = ('name', 'description')
     list_filter = ('created_at',)
+    # ENHANCEMENT: Allows quick editing of these crucial fields
+    list_editable = ('price', 'side_price', 'stock') 
+
 
 # -------------------------------
 # CartItem
